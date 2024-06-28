d14de662e5ef5bc895552108881408f4db25506a
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-06-28 13:39:10 +0530

    fix: cors error

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,19 +10,24 @@ const app = express();
 const port = process.env.PORT || 5001;
 app.use(express.json({ limit: "50mb" }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://article-arc.vercel.app",
+];
 
-app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
