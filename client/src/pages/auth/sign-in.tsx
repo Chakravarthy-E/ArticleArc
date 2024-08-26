f0ce1d5c91ae9858fcbfe944ab4c14f0c46bae5f
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-08-26 12:38:22 +0530

    :art:styles: fix the styles in auth pages

--- client/src/pages/auth/sign-in.tsx
+++ client/src/pages/auth/sign-in.tsx
@@ -66,8 +66,8 @@ export default function SignIn() {
         <link rel="icon" href="/blog.svg" />
       </Head>
 
-      <div className="flex items-center justify-center min-h-screen p-4 font-outfit bg-gray-100">
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <div className="flex items-center justify-center min-h-screen p-4 font-outfit ">
+        <div className=" p-8 rounded-lg shadow-md w-full max-w-md">
           <h1 className="text-2xl font-semibold mb-4 text-center">Sign In</h1>
           <div>
             <div className="mb-4">
--- client/src/pages/auth/sign-up.tsx
+++ client/src/pages/auth/sign-up.tsx
@@ -58,8 +58,8 @@ export default function SignUp() {
         <link rel="icon" href="/blog.svg" />
       </Head>
 
-      <div className="flex items-center justify-center min-h-screen p-4 bg-gray-100 font-outfit">
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <div className="flex items-center justify-center min-h-screen p-4  font-outfit">
+        <div className=" p-8 rounded-lg shadow-md w-full max-w-md">
           <h1 className="text-2xl font-semibold mb-4 text-center">Sign Up</h1>
           <div>
             <div className="mb-4">
